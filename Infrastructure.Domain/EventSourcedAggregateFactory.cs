cc1038c90e4e4e1fb633bf40bf14fa67e40a8efd
Tautvydas Versockas <tautvydas.versockas@gmail.com>
2020-03-02 15:01:19 +0200

    Tabs style fix.

--- Infrastructure.Domain/EventSourcedAggregateFactory.cs
+++ Infrastructure.Domain/EventSourcedAggregateFactory.cs
@@ -10,7 +10,7 @@ namespace Infrastructure.Domain
         {
             var aggregateType = typeof(TEventSourcedAggregate);
             var constructor = aggregateType.GetConstructor(new[] { typeof(Guid), typeof(IEnumerable<IVersionedEvent>) }) ??
-                              throw new InvalidCastException($"Type {aggregateType.Name} must have a constructor with the following signature: .ctor(Guid, IEnumerable<IVersionedEvent>)");
+                throw new InvalidCastException($"Type {aggregateType.Name} must have a constructor with the following signature: .ctor(Guid, IEnumerable<IVersionedEvent>)");
             return (TEventSourcedAggregate)constructor.Invoke(new object[] { id, events });
         }
     }
